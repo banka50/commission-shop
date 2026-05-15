e8ca34293e3abd7c473e46de0af4d49401682879
beato07 <apyuretskiy@ptsecurity.com>
2026-05-16 00:47:51 +0300

    Add PostgreSQL and MySQL support in configuration, update requirements, and enhance .env for database selection

--- src/config.py
+++ src/config.py
@@ -2,12 +2,33 @@ import os
 
 
 class Config:
-    USER = os.environ.get('POSTGRES_USER', 'postgres')
-    PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'root')
-    HOST = os.environ.get('POSTGRES_HOST', '127.0.0.1')
-    PORT = os.environ.get('POSTGRES_PORT', '5432')
-    DB = os.environ.get('POSTGRES_DB', 'db')
+    DB_ENGINE = os.environ.get('DB_ENGINE', 'postgresql')
 
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def _build_uri() -> str:
+        engine = os.environ.get('DB_ENGINE', 'postgresql')
+
+        if engine == 'postgresql':
+            user = os.environ.get('POSTGRES_USER', 'postgres')
+            password = os.environ.get('POSTGRES_PASSWORD', 'root')
+            host = os.environ.get('POSTGRES_HOST', '127.0.0.1')
+            port = os.environ.get('POSTGRES_PORT', '5432')
+            db = os.environ.get('POSTGRES_DB', 'db')
+            return f'postgresql://{user}:{password}@{host}:{port}/{db}'
+
+        if engine == 'mysql':
+            user = os.environ.get('MYSQL_USER', 'root')
+            password = os.environ.get('MYSQL_PASSWORD', 'root')
+            host = os.environ.get('MYSQL_HOST', '127.0.0.1')
+            port = os.environ.get('MYSQL_PORT', '3306')
+            db = os.environ.get('MYSQL_DB', 'db')
+            return f'mysql+pymysql://{user}:{password}@{host}:{port}/{db}'
+
+        # sqlite по умолчанию
+        db_path = os.environ.get('SQLITE_PATH', 'commission_shop.db')
+        return f'sqlite:///{db_path}'
+
+    SQLALCHEMY_DATABASE_URI = _build_uri.__func__()
